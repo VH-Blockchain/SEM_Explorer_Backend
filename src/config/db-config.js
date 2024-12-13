ed9d713202905abf534ab55bd7db0125e89daa45
Hitendrasinh Chauhan <hiten@virtualheight.com>
2024-12-13 20:39:13 +0530

    fixed envb

--- src/config/db-config.js
+++ src/config/db-config.js
@@ -11,7 +11,7 @@ export const connection = mysql.createConnection({
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME
 
-});
+});  N
 
 connection.connect(function (err) {
   if (err) {
